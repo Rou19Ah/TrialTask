d2db91c20c4edf7ddc59e54390f99ae38d1e670b
Ahmad Rouhani <126976556+Rou19Ah@users.noreply.github.com>
2023-12-08 08:02:07 +0100

    Delete TableDB.sql
    No need of this file anymore

--- TableDB.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO [dbo].[Lasers] ([LaserID], [LaserName], [MaximumPower], [MaximumWeldingDuration], [NumberOfTriggeredWelds], [ConsumedEnergy])
-VALUES
-    (1, 'Laser A', 150.00, 600, 100, 20.5),
-    (2, 'Laser B', 200.00, 800, 120, 25.0),
-    (3, 'Laser C', 180.00, 700, 110, 22.5);
